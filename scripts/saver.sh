d9796b77a70e9ac432060c68aeefe4f06dd7053f
Vlad <vlad10795@gmail.com>
2018-07-13 20:05:10 +0300

    Saver script

---
+++ scripts/saver.sh
@@ -0,0 +1,22 @@
+# append it to the history file
+append_current_line() {
+  if [ -n "$ZSH_VERSION" ]; then
+    local cur_line="$BUFFER"
+  elif [ -n "$BASH_VERSION" ]; then
+    local cur_line="$READLINE_LINE"
+  else
+   # asume something else, TODO
+  fi
+  local cur_line="$BUFFER" 
+  echo "$cur_line" >> ~/.commander/history
+}
+
+# create the history file
+mkdir -p ~/.commander
+touch ~/.commander/history
+
+# declare a new widget
+zle -N lookit
+# bind widget to Ctrl+k 
+bindkey '\ek' lookit
+
